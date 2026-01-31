4900d9ce6bcc3bbd01314cdbd50577ce67877eb3
yaseen <yaseen.aa@prolance.co.in>
2026-01-31 15:40:17 +0530

    moved login to pages

--- cc-app-server/routes/approver.route.js
+++ cc-app-server/routes/approver.route.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 const approverController =  require('../controllers/approver.controller.js');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
 router.post('/login', approverController.loginApprover);
 
 module.exports = router;
--- cc-app-ui/src/App.jsx
+++ cc-app-ui/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import ProtectedRoute from './utils/ProtectedRoute';
 import Login from './components/approver/Login';
+import ApproverLogin from './pages/ApproverLogin';
 
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
       {/* Public Routes */}
         {/* <Route path="/login" element={<Login />} /> */}
         <Route path="/" element={<HomePage />} />
-        <Route path="/approver/login" element={<Login />} />
+        <Route path="/approver/login" element={<ApproverLogin />} />
         <Route element={<ProtectedRoute />}>
           <Route path="/protected" element={<HomePage />} />
           <Route path="/approver/dashboard" element={<HomePage />} />
