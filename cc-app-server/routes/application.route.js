860e1a450499e0b9d39059fcce05602417d41626
ragusrinivasan <raguraam.s@outlook.com>
2026-01-31 15:23:44 +0530

    Add authentication middleware to GET all applications route

--- cc-app-server/routes/application.route.js
+++ cc-app-server/routes/application.route.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const  applicationController  = require('../controllers/application.controller');
+const applicationController = require('../controllers/application.controller');
+const authenticate = require('../middlewares/authenticate.middleware');
 
 /* GET all applications */
-router.get('/', applicationController.getAllApplications);
+router.get('/', authenticate, applicationController.getAllApplications);
 module.exports = router;
