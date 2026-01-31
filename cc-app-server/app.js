6de2adbcff3959d7ae07fbc1c63eb3aa730a07a1
yaseen <yaseen.aa@prolance.co.in>
2026-01-31 15:07:59 +0530

    axios setup done in client

--- cc-app-server/app.js
+++ cc-app-server/app.js
@@ -20,14 +20,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(cors());
+app.use(cors(["http://localhost:5173"]));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', applicationRouter);
-app.use('/approvers', approverRouter);
+app.use('/approver', approverRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,5 +44,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
